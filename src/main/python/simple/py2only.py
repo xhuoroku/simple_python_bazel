d275cb627d1c3298c9c55f9b2e08eda7529eaf23
Xiaoguang Huo <xhuo@roku.com>
2021-05-26 00:08:04 -0700

    remove par_binary and print python version

--- src/main/python/simple/py2only.py
+++ src/main/python/simple/py2only.py
@@ -1,8 +1,10 @@
 # import boto3
+import sys
 
 def main(): 
 	print("Python 2 only!")
 	print("yay done!")
+	print(sys.version)
 
 if __name__ == '__main__':
-  main()
+    main()
--- src/main/python/simple/py3only.py
+++ src/main/python/simple/py3only.py
@@ -1,13 +1,13 @@
 # import boto3
+import sys
 
 def python3_only_function(parameter, *, option1=False, option2=''):
-    pass
-
+	print(sys.version)
+    
 def main(): 
 	python3_only_function(3, option1=True, option2='Hello World!')
 	print("yay done!")
+    
 
 if __name__ == '__main__':
-  main()
-
-
+    main()
